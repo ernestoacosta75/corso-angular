c554334f7a59e4acb39f20fe871c0009c79ac766
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2020-04-17 18:26:59 +0200

    The Angular HTTP Client - Get calls with Request Parameters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +15,22 @@ import {Observable} from 'rxjs';
 export class AppComponent implements OnInit {
 
 
-  courses = COURSES;
+  courses: Array<Course>;
 
-  constructor() {
+  constructor(private http: HttpClient) {
 
   }
 
+  // To put some initialization logic, like triggering HTTP calls to the backend
   ngOnInit() {
+    const params = new HttpParams()
+    .set("page", "1")
+    .set("pageSize", "10");
+
+    this.http.get('/api/courses', {params}).pipe(
+      tap(val => console.log('http 0: ', val)),
+      map((val: Array<Course>) => val)
+    ).subscribe( val => this.courses = val);
   }
 
 
